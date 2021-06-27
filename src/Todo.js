f7957c561ace631aaf95bb8442a78306792280cf
Suyog Patil <suyog3198@gmail.com>
2021-06-27 18:38:34 +0530

    Changes made

--- src/Todo.js
+++ src/Todo.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './App.css'
 export default function Todo({text,todoArray,setTodoArray,todo,setEditItem,setTextInput,setLocalStorage}) {
     // const [deleteTodo,setDeleteTodo] = useState("");
+    const[isCompleted,setIsCompleted] = useState(false);
     const deleteTodo = () => {
         let afterDeletionArray = todoArray.filter((element) => element.id !== todo.id);
         setTodoArray(afterDeletionArray);
@@ -18,7 +19,10 @@ export default function Todo({text,todoArray,setTodoArray,todo,setEditItem,setTe
     const complete = () => {
         // let edit = ;
         // console.log(edit);
-        (todo.completed===true) ? document.getElementById("edit").disabled = true : document.getElementById("edit").disabled = false;
+
+        (todo.completed===true) ? setIsCompleted(true) : setIsCompleted(false);
+
+        // (todo.completed===true) ? document.getElementById("edit").disabled = true : document.getElementById("edit").disabled = false;
         let completedTodo = todoArray.map((el) => {
            if (el.id === todo.id)  {
                return{
@@ -34,11 +38,12 @@ export default function Todo({text,todoArray,setTodoArray,todo,setEditItem,setTe
         <div>
         <div className={(todo.completed) ? "todo_checked" : "todo"} >
             <div className="todoText">
+                <button onClick={complete} id="check" ><i className="fas fa-check"></i></button>
                 <li className="text">{text}</li>
             </div> 
             <div className="buttons">
-                <button onClick={complete} id="check" ><i className="fas fa-check"></i></button>
-                <button onClick={editTodo} disabled={todo.completed} id = "edit" className = "edit"><i className="fas fa-edit "></i></button>
+                
+                <button onClick={editTodo} disabled={todo.completed} id = "edit" className = {isCompleted ? "editDisabled" : "edit"}><i className="fas fa-edit "></i></button>
                 <button onClick = {deleteTodo} id = "trash"><i className="fas fa-trash"></i></button>
             </div>
         </div>
